40817cc8b866ad586db8abbb7c241fecd58f4457
Chris McDonough <chrism@plope.com>
2011-06-04 20:09:23 -0400

    prep for 0.13

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ copyright = '2008, Repoze Developers <repoze-dev@lists.repoze.org>'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.12'
+version = '0.13'
 # The full version, including alpha/beta/rc tags.
 release = version
 
